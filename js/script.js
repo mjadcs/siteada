b05e76feda3029bbe9472054179580c617cd5e8e
mjadcs <mariajuliaadcs@gmail.com>
2020-11-26 17:29:47 -0300

    Apaga campos após envio de dados preenchidos no form

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-document.getElementById("botaoEnviar").addEventListener("click", envioFormulario) 
+var elemBotaoEnviar = document.getElementById("botaoEnviar")
+var elemNomeUsuaria = document.getElementById("nome-da-usuaria")
+var elemEmailUsuaria = document.getElementById("email-da-usuaria")
+var elemTelUsuaria = document.getElementById("telefone-da-usuaria")
 
-function envioFormulario(){
+elemBotaoEnviar.addEventListener("click", envioFormulario) 
 
-  if(document.getElementById("nome-da-usuaria").value != "" &&
-  document.getElementById("email-da-usuaria").value != "" &&
-  document.getElementById("telefone-da-usuaria").value != ""){
-      alert("Prontinho! Você receberá no seu email! ")
-  }else{
-      alert("Por favor, preencha os campos nome e email")
+function envioFormulario() {
+  if (elemNomeUsuaria.value != "" && elemEmailUsuaria.value != "" && elemTelUsuaria.value != "") {
+    alert("Prontinho! Você receberá no seu email! ")
+    elemNomeUsuaria.value = ""
+    elemEmailUsuaria.value = ""
+    elemTelUsuaria.value = ""
+  } else {
+    alert("Por favor, preencha os campos nome e email")
   }
 }
